d0931866b9d544efa3aad574e2588c3c14114f2e
xu.lei@wuqi-tech.com <xu.lei@wuqi-tech.com>
2021-01-19 16:23:20 +0800

    初始化项目

---
+++ README.md
@@ -0,0 +1,4 @@
+# 安装依赖
+npm i
+# 本地开发 启动项目
+npm run serve
---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="vue-base-project">
+    <router-view />
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    mounted() {},
+  }
+</script>
